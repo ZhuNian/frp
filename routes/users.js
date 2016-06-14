ad0c05251318a735b086834176a763b38c425423
zhun <zhun@huoli.com>
2016-06-14 18:55:36 +0800

    frp test

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var stream = require('r-stream');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  var number = stream(5);
+
+  console.log(number());
+
+  number(7);
+
+  res.render('users', {
+    title: 'user page',
+    name: 'zhunian'
+  });
 });
 
 module.exports = router;
